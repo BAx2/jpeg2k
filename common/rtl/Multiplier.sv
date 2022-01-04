b32f581606dc0fbcd96fd330e6184e845a415087
B4xZ <antosh-ka2000@list.ru>
2022-01-04 16:50:58 +0300

    fix: mult slice

--- common/rtl/Multiplier.sv
+++ common/rtl/Multiplier.sv
@@ -12,5 +12,13 @@ module Multiplyer #(
     logic signed [Width*2 - 1:0] m;
     assign m = a_i * b_i;
 
-    assign m_o = m[15+LowSignificantBit:LowSignificantBit];
+    assign m_o = m[Width+LowSignificantBit-1:LowSignificantBit];
+
+    // sim 
+    real ra, rb, rmi, rm;
+    assign ra  = a_i    / (2.0 ** InPoint);
+    assign rb  = b_i    / (2.0 ** InPoint);
+    assign rmi = m      / (2.0 ** (2 * InPoint));
+    assign rm  = m_o    / (2.0 ** OutPoint);
+
 endmodule
